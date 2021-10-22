7d95e4fbdc90a656a4a8f9a27c0137ea8319c45f
sakilk130 <sakilk130@gmail.com>
2021-10-22 20:57:35 +0600

    complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import '../styles/Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import { Grid, Slider } from '@material-ui/core';
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://static.remove.bg/remove-bg-web/99ab439c6e8e26adfd8c59cee4eb26f0376a9584/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/helpers/reducer.js
+++ src/helpers/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlist: [],
   playing: false,
   item: null,
-  //   token:
-  //     'BQAYqJhIrDxOaatqSqEBNt0UODDGab2s2fbknkBz2PHjv7d0wEnXnC2wJCR1DJ5q6k65ckMtd_M9h5kOnOJgNpi23PAOJuEFJ5tMGqAgWY-ySaIFiwQ1G4WP0Fwb8kx8I14ud4w05V3pNTTvIU2B6EhoEv7HXdQ52CaSlvBTu6z6c75u',
+  token:
+    'BQAYqJhIrDxOaatqSqEBNt0UODDGab2s2fbknkBz2PHjv7d0wEnXnC2wJCR1DJ5q6k65ckMtd_M9h5kOnOJgNpi23PAOJuEFJ5tMGqAgWY-ySaIFiwQ1G4WP0Fwb8kx8I14ud4w05V3pNTTvIU2B6EhoEv7HXdQ52CaSlvBTu6z6c75u',
 };
 
 const reducer = (state, action) => {
